3d1c1577351d7b23f754e78a56734daaedcd9d56
leo_physics <leyou@umich.edu>
2025-01-05 18:43:42 -0500

    update OpenFRET version for python library

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="openfret",
-    version="0.1.0",
+    version="0.1.1",
     author="Jieming Li",
     author_email="jmli@umich.edu",
     description="A standardized format for single-molecule FRET data",
