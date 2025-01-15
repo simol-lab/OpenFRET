366b4fdc7dec94cc7f437870e02849ddb9496b66
Alex Johnson-Buck <alebuck@umich.edu>
2025-01-15 14:42:30 -0500

    Matlab: conform function names with python + tweaks to main.m script.

--- matlab/main.m
+++ matlab/main.m
@@ -1,4 +1,6 @@
 % Create a sample dataset
+save_name = 'fret_data.json';
+
 dataset.title = 'My FRET Experiment';
 dataset.description = 'Example FRET data';
 dataset.experiment_type = '2-Color FRET';
@@ -14,21 +16,21 @@ dataset.instrument_details.detector = 'EMCCD Camera';
 dataset.instrument_details.other_details.objective = '100x';
 
 trace1.channels(1).channel_type = 'donor';
-trace1.channels(1).data = rand(1, 1000);
+trace1.channels(1).data = rand(1, 100);
 trace1.channels(1).excitation_wavelength = 532;
 trace1.channels(1).emission_wavelength = 580;
 trace1.channels(2).channel_type = 'acceptor';
-trace1.channels(2).data = rand(1, 1000);
+trace1.channels(2).data = rand(1, 100);
 trace1.channels(2).excitation_wavelength = 532;
 trace1.channels(2).emission_wavelength = 680;
 trace1.metadata.trace_id = 'trace001';
 
 trace2.channels(1).channel_type = 'donor';
-trace2.channels(1).data = rand(1, 1000);
+trace2.channels(1).data = rand(1, 100);
 trace2.channels(1).excitation_wavelength = 532;
 trace2.channels(1).emission_wavelength = 580;
 trace2.channels(2).channel_type = 'acceptor';
-trace2.channels(2).data = rand(1, 1000);
+trace2.channels(2).data = rand(1, 100);
 trace2.channels(2).excitation_wavelength = 532;
 trace2.channels(2).emission_wavelength = 680;
 trace2.metadata.trace_id = 'trace002';
@@ -36,10 +38,17 @@ trace2.metadata.trace_id = 'trace002';
 dataset.traces = [trace1, trace2];
 
 % Write the dataset to a JSON file
-openfret.write(dataset, 'fret_data.json');
+openfret.write_data(dataset, save_name);
 
-% Read the dataset back from the JSON file
-loaded_dataset = openfret.read('fret_data.json');
+
+% Read dataset from a JSON file
+load_name = 'fret_data.json';
+
+loaded_dataset = openfret.read_data(load_name);
 
 % Compare the original and loaded datasets (basic check)
-isequal(dataset.title, loaded_dataset.title)
+if isequal(dataset.title, loaded_dataset.title)
+    disp('Success! Dataset was created and loaded.');
+else
+    disp('Error: Something went wrong. Check that the save name and load name are identical.')
+end
